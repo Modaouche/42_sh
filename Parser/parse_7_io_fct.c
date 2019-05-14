058adceaa03a578fc4d6a275a612c305987d1182
mohada <modaouch@student.42.fr>
2019-05-14 13:39:47 +0200

    small parser issue fixed

--- Parser/parse_7_io_fct.c
+++ Parser/parse_7_io_fct.c
@@ -6,7 +6,7 @@
 /*   By: modaouch <modaouch@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/07 00:38:00 by modaouch          #+#    #+#             */
-/*   Updated: 2019/05/14 13:02:37 by modaouch         ###   ########.fr       */
+/*   Updated: 2019/05/14 13:33:32 by modaouch         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,12 @@
 
 void        io_redirect_fct(t_ast **ast, t_edit *line_e)
 {
-    if (first_set(head_of_line(*ast), T_IO_NB, -1))
+    if (first_set(head_of_line(*ast), T_GREAT, T_GREATAND, T_DGREAT,T_IO_NB,\
+         T_CLOBBER, T_LESSGREAT, T_LESS, T_DLESS, T_LESSAND, T_DLESSDASH, -1))
+    {
         io_number_opt_fct(ast, line_e);
-    else if (first_set(head_of_line(*ast), T_GREAT, T_GREATAND, T_DGREAT,\
-        T_CLOBBER, T_LESSGREAT, T_LESS, T_DLESS, T_LESSAND, T_DLESSDASH, -1))
         io_kind_fct(ast, line_e);
+    }
 }
 
 void        io_number_opt_fct(t_ast **ast, t_edit *line_e)
@@ -84,7 +85,7 @@ void        io_here(t_ast **ast, t_edit *line_e)
         }
         else
         {
-            ast_insert_right(heredoc, ast);
+            ast_insert_right(heredoc, ast);//!//
 	        ft_printf("|%d|\n", head_of_line(*ast));
             ft_putstr_fd("42sh syntax error.13\n", 2);//check si je rentre, peut etre que c'est inutile avec specifical message
             g_errorno = ER_SYNTAX;
