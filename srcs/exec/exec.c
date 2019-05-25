9c6cfa7fa7bc85cb3b5ebc3e5c93eef3f4623a20
mohammed <mo.daouche@gmail.com>
2019-05-25 03:08:48 +0200

    debut de la branch modaouch et l'execution des commandes

---
+++ srcs/exec/exec.c
@@ -0,0 +1,19 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   exit_le.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: modaouch <modaouch@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/02/06 11:57:20 by modaouch          #+#    #+#             */
+/*   Updated: 2019/02/06 18:19:03 by modaouch         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "shell.h"
+
+/*Debut de l'execution, des built-ins, | et redirections... , s'inspirer de minishell pour certains builtins*/
+
+... exec(t_ast..., t_edit *line_e)
+{
+}	
