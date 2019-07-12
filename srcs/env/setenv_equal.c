2d0e509f56599f95f8525deadaf4dde57de7eca0
Arthur RAOUT <araout@e1r10p10.42.fr>
2019-07-12 05:44:34 +0200

    fixed  bug where every word typed was added to intern env without checking equal sign

--- srcs/env/setenv_equal.c
+++ srcs/env/setenv_equal.c
@@ -6,7 +6,7 @@
 /*   By: araout <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/09 22:43:17 by araout            #+#    #+#             */
-/*   Updated: 2019/07/12 04:12:37 by araout           ###   ########.fr       */
+/*   Updated: 2019/07/12 05:41:17 by araout           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,13 +19,13 @@ char		*get_value(char *cmd)
 	i = 0;
 	while (cmd[i])
 	{
-		if (cmd[i] == '=' && cmd[i + 1])
+		if (cmd[i] == '=')
 			return (&(cmd[i + 1]));
 		else if (cmd[i] == '=' && !cmd[i + 1])
 			return ("");
 		i++;
 	}
-	return ("");
+	return (NULL);
 }
 
 char		*get_varname(char *cmd)
@@ -49,14 +49,14 @@ char		*get_varname(char *cmd)
 	return (ft_strdup(cmd));
 }
 
-int			ft_export_inner_var(char *cmd)
+int			ft_export_inner_var(char *cmd, int flag)
 {
 	char	*value;
 
 	value = NULL;
 	if (!g_shell.intern_var)
 		return (0);
-	else if (g_shell.intern_var[find_var(cmd, g_shell.intern_var)])
+	else if (g_shell.intern_var[find_var(cmd, g_shell.intern_var)] && flag == 1)
 	{
 		value = get_env_value(cmd);
 		ft_unsetenv(cmd);
@@ -77,7 +77,7 @@ int			ft_setenv_equal(char *cmd, int flag)
 	if (flag == 1 && value && ft_unsetenv(varname))
 		g_shell.envp = set_var_env(varname, value, g_shell.envp);
 	else if (flag == 1 && !value)
-		ft_export_inner_var(cmd);
+		ft_export_inner_var(cmd, flag);
 	else if (flag == 0 && value)
 	{
 		if (g_shell.envp[find_var(cmd, g_shell.envp)])
@@ -86,7 +86,7 @@ int			ft_setenv_equal(char *cmd, int flag)
 			ft_strdel(&varname);
 			return (1);
 		}
-		else
+		else if ((value = get_value(cmd)))
 			g_shell.intern_var = set_var_env(varname,\
 					value, g_shell.intern_var);
 	}
