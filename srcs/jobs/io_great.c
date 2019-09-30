3332e1406100532216c54b42e05ab473d7596d45
Kiloutre <kilowwwww@gmail.com>
2019-09-30 02:11:21 +0200

    Momo stuff

--- srcs/jobs/io_great.c
+++ srcs/jobs/io_great.c
@@ -15,93 +15,83 @@
 
 char    *ast_get_lexeme(t_ast *ast)
 {
-    ast = ast->right;
-    while (ast && ast->left)
-        ast = ast->left;
-    return (ast->token->lexeme);
+	ast = ast->right;
+	while (ast && ast->left)
+		ast = ast->left;
+	return (ast->token->lexeme);
 }
+
 
 void    io_great(t_ast *ast, t_job *j)
 {
-    int fd;
+	int fd;
+	int io_nb;
 
-    ft_printf("===GREAT===\n");
-    if ((fd = open(ast_get_lexeme(ast), O_CREAT | O_TRUNC | O_WRONLY, 0644)) < 0)
-    {
-        access_verification(ast_get_lexeme(ast));
-        return ;
-    }
-    if (ast->left->token->tokind == T_IO_NB)
-    {
-        if (ast->left->token->tokind - 48 == 0)
-            check_opened_fd(j, 0, fd);
-        if (ast->left->token->tokind - 48 == 1)
-            check_opened_fd(j, 1, fd);
-        if (ast->left->token->tokind - 48 == 2)
-            check_opened_fd(j, 2, fd);
-        return ;
-    }
-    check_opened_fd(j, 0, fd);
-    j->stdout = fd;
+
+	if ((fd = open(ast_get_lexeme(ast), O_CREAT | O_TRUNC | O_WRONLY, 0644)) < 0)
+	{
+		access_verification(ast_get_lexeme(ast));
+		return ;
+	}
+	if (ast->left->token->tokind == T_IO_NB)
+	{
+		io_nb = ft_atoi(ast->left->token->lexeme);
+		return (check_opened_fd(j, io_nb, fd));
+	}
+	check_opened_fd(j, 0, fd);
+	j->stdout = fd;
 }
 
 void    io_dgreat(t_ast *ast, t_job *j)
 {
-    int fd;
+	int fd;
+	int io_nb;
 
-    ft_printf("===DGREAT===\n");
-    if ((fd = open(ast_get_lexeme(ast), O_CREAT | O_WRONLY, 0644)) < 0)
-    {
-        access_verification(ast_get_lexeme(ast));
-        return ;
-    }
-    lseek(fd, 0, SEEK_END);
-    if (ast->left->token->tokind == T_IO_NB)
-    {
-        if (ast->left->token->tokind - 48 == 0)
-            check_opened_fd(j, 0, fd);
-        if (ast->left->token->tokind - 48 == 1)
-            check_opened_fd(j, 1, fd);
-        if (ast->left->token->tokind - 48 == 2)
-            check_opened_fd(j, 2, fd);
-        return ;
-    }
-    check_opened_fd(j, 0, fd);
-    j->stdout = fd;
+	ft_printf("===DGREAT===\n");
+	if ((fd = open(ast_get_lexeme(ast), O_CREAT | O_WRONLY, 0644)) < 0)
+	{
+		access_verification(ast_get_lexeme(ast));
+		return ;
+	}
+	lseek(fd, 0, SEEK_END);
+	if (ast->left->token->tokind == T_IO_NB)
+	{
+		io_nb = ft_atoi(ast->left->token->lexeme);
+		return (check_opened_fd(j, io_nb, fd));
+	}
+	check_opened_fd(j, 0, fd);
+	j->stdout = fd;
 }
 
 void    io_greatand(t_ast *ast, t_job *j)
 {
-    int fd;
+	int fd;
+	int io_nb;
 
-    ft_printf("===GREATAND===\n");
-    fd = atoi(ast_get_lexeme(ast));
-    if (ast->left->token->tokind == T_IO_NB)
-    {
-        if (ast->left->token->tokind - 48 == 0)
-            check_opened_fd(j, 0, fd);
-        if (ast->left->token->tokind - 48 == 1)
-            check_opened_fd(j, 1, fd);
-        if (ast->left->token->tokind - 48 == 2)
-            check_opened_fd(j, 2, fd);
-        return ;
-    }
-    check_opened_fd(j, 0, fd);
-    j->stdout = fd;
+	ft_printf("===GREATAND===\n");
+	fd = atoi(ast_get_lexeme(ast));
+	if (ast->left->token->tokind == T_IO_NB)
+	{
+		io_nb = ft_atoi(ast->left->token->lexeme);
+		return (check_opened_fd(j, io_nb, fd));
+		return ;
+	}
+	check_opened_fd(j, 0, fd);
+	j->stdout = fd;
 }
 
 void    io_lessgreat(t_ast *ast, t_job *j)
 {
-    (void)ast;
-    (void)j;
-    ft_printf_fd(STDERR_FILENO, "./42sh: This feature is not supported.");
-    g_shell.errorno = ER_UNKNOW;
+	(void)ast;
+	(void)j;
+	ft_printf_fd(STDERR_FILENO, "./42sh: This feature is not supported.");
+	g_shell.errorno = ER_UNKNOW;
 }
 
 void    io_clobber(t_ast *ast, t_job *j)
 {
-    (void)ast;
-    (void)j;
-    ft_printf_fd(STDERR_FILENO, "./42sh: This feature is not supported.");
-    g_shell.errorno = ER_UNKNOW;
+	(void)ast;
+	(void)j;
+	ft_printf_fd(STDERR_FILENO, "./42sh: This feature is not supported.");
+	g_shell.errorno = ER_UNKNOW;
 }
--- srcs/jobs/io_less.c
+++ srcs/jobs/io_less.c
@@ -16,92 +16,83 @@
 
 void    check_opened_fd(t_job *j, int btn, int fd)
 {
-    if (j->stdin != STDIN_FILENO && btn == 0)
-    {
-        close(j->stdin);
-        j->stdin = fd;
-    }
-    if (j->stdout != STDOUT_FILENO && btn == 1)
-    {
-        close(j->stdout);
-        j->stdout = fd;
-    }
-    if (j->stderr != STDERR_FILENO && btn == 2)
-    {
-        close(j->stderr);
-        j->stderr = fd;
-    }
+
+	//LOGIQUEMENT TU REDIRIGE DANS TOUS LES CAS BIBICHE
+	if (j->stdin == STDIN_FILENO && btn == 0)
+	{
+		close(j->stdin);
+		j->stdin = fd;
+	}
+	if (j->stdout == STDOUT_FILENO && btn == 1)
+	{
+		close(j->stdout);
+		j->stdout = fd;
+	}
+	if (j->stderr == STDERR_FILENO && btn == 2)
+	{
+		close(j->stderr);
+		j->stderr = fd;
+	}
 }
 
 void    io_less(t_ast *ast, t_job *j)
 {
-    int fd;
+	int fd;
+	int io_nb;
 
-    ft_printf("===LESS===\n");
-    if ((fd = open(ast_get_lexeme(ast), O_CREAT | O_WRONLY, 0644)) < 0)
-    {
-        access_verification(ast_get_lexeme(ast));
-        return ;
-    }
-    if (ast->left->token->tokind == T_IO_NB)
-    {
-        if (ast->left->token->tokind - 48 == 0)
-            check_opened_fd(j, 0, fd);
-        if (ast->left->token->tokind - 48 == 1)
-            check_opened_fd(j, 1, fd);
-        if (ast->left->token->tokind - 48 == 2)
-            check_opened_fd(j, 2, fd);
-        return ;
-    }
-    check_opened_fd(j, 0, fd);
-    j->stdin = fd;
+	ft_printf("===LESS===\n");
+	if ((fd = open(ast_get_lexeme(ast), O_CREAT | O_WRONLY, 0644)) < 0)
+	{
+		access_verification(ast_get_lexeme(ast));
+		return ;
+	}
+	if (ast->left->token->tokind == T_IO_NB)
+	{
+		io_nb = ft_atoi(ast->left->token->lexeme);
+		return (check_opened_fd(j, io_nb, fd));
+	}
+	check_opened_fd(j, 0, fd);
+	j->stdin = fd;
 }
 
 void    io_dless(t_ast *ast, t_job *j)
 {
-    int     fd;
-    char    *lex;
+	int     fd;
+	char    *lex;
+	int	io_nb;
 
-    ft_printf("===DLESS===\n");
-    lex = generate_random_filename("/tmp/dless_file");
-    if ((fd = open(lex, O_CREAT | O_RDWR, 0644)) < 0)
-    {
-        ft_strdel(&lex);
-        g_shell.errorno = ER_UNKNOW;
-        return ;
-    }
-    unlink(lex);
-    ft_strdel(&lex);
-    lex = ast_get_lexeme(ast);
-    write(fd, lex, ft_strlen(lex));
-    if (ast->left->token->tokind == T_IO_NB)
-    {
-        if (ast->left->token->tokind - 48 == 0)
-            check_opened_fd(j, 0, fd);
-        if (ast->left->token->tokind - 48 == 1)
-            check_opened_fd(j, 1, fd);
-        if (ast->left->token->tokind - 48 == 2)
-            check_opened_fd(j, 2, fd);
-        return ;
-    }
-    check_opened_fd(j, 0, fd);
+	ft_printf("===DLESS===\n");
+	lex = generate_random_filename("/tmp/dless_file");
+	if ((fd = open(lex, O_CREAT | O_RDWR, 0644)) < 0)
+	{
+		ft_strdel(&lex);
+		g_shell.errorno = ER_UNKNOW;
+		return ;
+	}
+	unlink(lex);
+	ft_strdel(&lex);
+	lex = ast_get_lexeme(ast);
+	write(fd, lex, ft_strlen(lex));
+	if (ast->left->token->tokind == T_IO_NB)
+	{
+		io_nb = ft_atoi(ast->left->token->lexeme);
+		return (check_opened_fd(j, io_nb, fd));
+		return ;
+	}
+	check_opened_fd(j, 0, fd);
 }
 
 void    io_lessand(t_ast *ast, t_job *j)
 {
-    int fd;
+	int fd;
+	int io_nb;
 
-    ft_printf("===LESSAND===\n");
-    fd = atoi(ast_get_lexeme(ast));
-    if (ast->left->token->tokind == T_IO_NB)
-    {
-        if (ast->left->token->tokind - 48 == 0)
-            check_opened_fd(j, 0, fd);
-        if (ast->left->token->tokind - 48 == 1)
-            check_opened_fd(j, 1, fd);
-        if (ast->left->token->tokind - 48 == 2)
-            check_opened_fd(j, 2, fd);
-        return ;
-    }
-    check_opened_fd(j, 0, fd);
+	ft_printf("===LESSAND===\n");
+	fd = atoi(ast_get_lexeme(ast));
+	if (ast->left->token->tokind == T_IO_NB)
+	{
+		io_nb = ft_atoi(ast->left->token->lexeme);
+		return (check_opened_fd(j, 2, fd));
+	}
+	check_opened_fd(j, 0, fd);
 }
