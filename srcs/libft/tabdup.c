2ad8607f175729f4bdc677e6eaf1e59b05ae6b9a
Kiloutre <kilowwwww@gmail.com>
2019-09-30 04:28:52 +0200

    Fix tabdup

--- srcs/libft/tabdup.c
+++ srcs/libft/tabdup.c
@@ -26,24 +26,20 @@ static char **exit_free_prev(char **tab, size_t len)
 char		**tabdup(char **tab)
 {
     char	**new;
-    char    *snew;
     size_t  len;
     size_t  i;
 
     if (!tab || !*tab)
         return (0);
     len = ft_tablen(tab);
-    i = 0;
     if (!(new = (char **)ft_memalloc(sizeof(char *) * (len + 1))))
         return (0);
-    len = 0;
+    i = 0;
     while (i < len)
     {
-        if (!(snew = strdup(tab[i])))
+        if (!(new[i] = ft_strdup(tab[i])))
             return (exit_free_prev(new, i));
-        new[i] = snew;
-        i++;
+        ++i;
     }
-    new[len] = NULL;
     return (new);
 }
